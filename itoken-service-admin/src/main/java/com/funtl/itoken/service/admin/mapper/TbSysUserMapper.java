244e12e2bb8dd9286e3a20dd7cff92710b858d9d
zzkkxx1020 <zzkkxx1020@163.com>
2019-11-12 21:57:20 +0800

    结构基本完成

---
+++ itoken-service-admin/src/main/java/com/funtl/itoken/service/admin/mapper/TbSysUserMapper.java
@@ -0,0 +1,7 @@
+package com.funtl.itoken.service.admin.mapper;
+
+import com.funtl.itoken.service.admin.domain.TbSysUser;
+import tk.mybatis.mapper.MyMapper;
+
+public interface TbSysUserMapper extends MyMapper<TbSysUser> {
+}
